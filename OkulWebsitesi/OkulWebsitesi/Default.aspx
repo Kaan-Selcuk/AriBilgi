557e23a0a54fa8756310e97f45fddc998d118c27
Kaan <teroniko98@gmail.com>
2025-04-28 21:21:08 +0300

    OkulWebsitesi_Eklendi

---
+++ OkulWebsitesi/OkulWebsitesi/Default.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="OkulWebsitesi.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        
+            <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+        <br>
+            <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+        <br>
+            <asp:Button ID="EklemeButonu" runat="server" OnClick="EklemeButonu_Click" Text="Button" />
+        
+            <asp:GridView ID="GridView1" runat="server">
+            </asp:GridView>
+        
+    </form>
+</body>
+</html>
---
+++ OkulWebsitesi/OkulWebsitesi/Default.aspx.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace OkulWebsitesi
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            con = new SqlConnection();
+            con.ConnectionString = "Data Source=KSK\\SQLKAAN;Initial Catalog=Okul;Integrated Security=True;";
+            con.Open();
+            UpdateTable();
+            con.Close();
+        }
+        private SqlConnection con;
+        protected void EklemeButonu_Click(object sender, EventArgs e)
+        {
+
+
+            con.Open();
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = con;
+            cmd.CommandText = "insert into Ogrenci(ogrenciNo,adSoyad) values (@ogrenciNo,@adSoyad)";
+
+
+            cmd.Parameters.AddWithValue("@ogrenciNo", TextBox1.Text);
+            cmd.Parameters.AddWithValue("@adSoyad", TextBox2.Text);
+
+            if (cmd.ExecuteNonQuery() > 0)
+            {
+                UpdateTable();
+            }
+
+            con.Close();
+        }
+        private void UpdateTable()
+        {
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter("select *  from Ogrenci", con);
+
+            DataTable dt = new DataTable();
+
+            sqlDataAdapter.Fill(dt);
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+            
+        }
+        
+    }
+}
